a8e92952d56ec8311749665b0f2e44bc0c87478b
Erich-VDB <85136309+Erich-VDB@users.noreply.github.com>
2023-02-25 15:32:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-var arrMovie = [];
-
-for (var i=0; i<4; i++){
-    arrMovie.push(prompt("Enter a four items on your shopping list: ", ""));
-}
-
-arrMovie.sort();
-
-for(var i=0; i<arrMovie.length; i++){
-    document.getElementById("test").innerHTML += arrMovie[i] + "<BR>";
-}
-
-function clearList(){
-    document.getElementById("test").innerHTML = "";
+//this function gathers the input 
+function processForm(){
+    //make the div visible 
+    document.getElementById("userInfo").style.display = "";
+    //first name 
+    document.getElementById("firstName").innerText = document.getElementById("fname").value;
+    //last name 
+    document.getElementById("lastName").innerText = document.getElementById("lname").value;
+    //age
+    document.getElementById("theAge").innerText = document.getElementById("age").value;
+    //email
+    document.getElementById("theEmail").innerText = document.getElementById("email").value;
 }
