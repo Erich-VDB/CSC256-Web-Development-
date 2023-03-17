1c863f1f936ea23c3aa5e4414398d55bab629678
Erich-VDB <85136309+Erich-VDB@users.noreply.github.com>
2023-03-16 23:27:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-//this function gathers the input 
-function processForm(){
-    //make the div visible 
-    document.getElementById("userInfo").style.display = "";
-    //first name 
-    document.getElementById("firstName").innerText = document.getElementById("fname").value;
-    //last name 
-    document.getElementById("lastName").innerText = document.getElementById("lname").value;
-    //age
-    document.getElementById("theAge").innerText = document.getElementById("age").value;
-    //email
-    document.getElementById("theEmail").innerText = document.getElementById("email").value;
-}
+
+var chessboard = document.getElementById("chessboard")
+//loop that makes each row. It runs 8 times
+    for (var i = 0; i < 8; i++){
+        //creates collums 8 times 
+        for (var j =0; j < 8; j++){
+            var chessSquare = document.createElement('div');
+            chessSquare.className = "chess-square";
+            if ((i+j) % 2 == 0){
+                chessSquare.style.backgroundColor = "green";
+            }
+            chessboard.appendChild(chessSquare);
+        }
+    }
---
+++ readme.txt
@@ -0,0 +1,5 @@
+there is one file labeled main.html. 
+The stlye portion can be charnged to enlarge the chessbaord 
+The Script portion can be altered to change the board's length and width 
+
+hope you enjoy this chess board 
